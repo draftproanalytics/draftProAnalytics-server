ff6c5e33c4117b70e96bc8261f060220102e2087
Darryl Thompson <dthompson98@gmail.com>
2026-07-12 14:57:53 -0500

    Fix runtime compile errors

---
+++ scripts/restoreDBScript.sql
@@ -0,0 +1,8 @@
+mysql \
+  -u dpa_user99 \
+  -p \
+  --database=MyNFL \
+  < /var/www/draftproanalytics-server/scripts/dpaBackup07122026.1414.sql
+
+
+DATABASE_URL=mysql://dpa_user99:uB402%4025%21@localhost:3306/MyNFL?allowPublicKeyRetrieval=true
